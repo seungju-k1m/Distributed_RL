376a3405d370b2375865cd760434dcf2270d629a
seungju-mmc <ksj970213@hanyang.ac.kjr>
2021-03-14 14:10:32 +0900

    delete

--- tesdtac.py
+++
@@ -1,7 +0,0 @@
-from SAC.Config import SACConfig
-from SAC.Player import sacPlayer
-
-
-config = SACConfig('./cfg/SAC.json')
-x = sacPlayer(config)
-x.run()
--- test.py
+++
@@ -1,7 +0,0 @@
-from SAC.Config import SACConfig
-from SAC.Learner import Learner
-
-
-config = SACConfig('./cfg/SAC.json')
-trainer = Learner(config)
-trainer.run()
