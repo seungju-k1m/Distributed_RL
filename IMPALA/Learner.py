317c228bf92ceb23c2e19828bc0bc97920e7b9f5
seungju-mmc <ksj970213@hanyang.ac.kr>
2021-03-25 23:52:09 +0900

    .

--- IMPALA/Learner.py
+++ IMPALA/Learner.py
@@ -265,9 +265,9 @@ class Learner:
                 self.writer.add_scalar("training_Time", time.time() - t, step)
 
     def step(self, step):
-        norm_gradient = self.model.clippingNorm(self.config.gradientNorm)
+        self.model.clippingNorm(self.config.gradientNorm)
         self.mOptim.step()
-        # norm_gradient = self.model.calculateNorm().cpu().detach().numpy()
+        norm_gradient = self.model.calculateNorm().cpu().detach().numpy()
 
         for g in self.mOptim.param_groups:
             g["lr"] = self.lr - self.step_delta * step
