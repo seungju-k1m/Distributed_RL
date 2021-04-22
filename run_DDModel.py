17d3d00c85e86f6456198a0b23f02ce265efc0dd
seungju-mmc <ksj970213@hanyang.ac.kr>
2021-04-22 17:08:46 +0900

    .

---
+++ run_DDModel.py
@@ -0,0 +1,33 @@
+import argparse
+
+from DDModel.Learner import Learner
+from DDModel.Config import DDModelConfig
+
+
+parser = argparse.ArgumentParser(description="Deep Dyanmics Model")
+parser.add_argument(
+    "--path",
+    "-p",
+    type=str,
+    default="./cfg/DDModel.json",
+    help="The path of configuration file.",
+)
+
+parser.add_argument(
+    "--train",
+    "-t",
+    action="store_true",
+    default=True,
+    help="if you add this arg, train mode is on",
+)
+
+args = parser.parse_args()
+
+
+if __name__ == "__main__":
+
+    path = args.path
+    cfg = DDModelConfig(path)
+    learner = Learner(cfg)
+    if args.train:
+        learner.run()
--- test__.py
+++
@@ -1,9 +0,0 @@
-from DDModel.Learner import Learner
-from DDModel.Config import DDModelConfig
-
-
-path = './cfg/DDModel.json'
-cfg = DDModelConfig(path)
-learner = Learner(cfg)
-# learner.GMP()
-learner.run()
